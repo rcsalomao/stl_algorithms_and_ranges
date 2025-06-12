ad9bcfc1b6d41e9b3196737f37fbbb30e5b777ee
rcsalomao <rafael.c.salomao@gmail.com>
2025-06-11 23:10:26 -0300

    chore: adjust build

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 export LD_LIBRARY_PATH="$HOME"/.local/lib/:"$GCC_DIR"/lib64/:"$LD_LIBRARY_PATH"
 
-cmake -S . -B ./build -DCMAKE_CXX_COMPILER="$GCC_DIR"/bin/g++ -DCMAKE_C_COMPILER="$GCC_DIR"/bin/gcc
-make -C ./build -j8 --silent
+cmake -S . -B ./build -DCMAKE_CXX_COMPILER="$GCC_DIR"/bin/g++
+make -C ./build -j"$(nproc)" --silent
 ./build/stl_algorithms_and_ranges
